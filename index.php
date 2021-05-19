911c0c4175630388a5309b2fed1e42e8d2924309
Tim McGinley <timwasnever@gmail.com>
2021-05-19 10:10:01 +0200

    comments

--- index.php
+++ index.php
@@ -1,17 +1,16 @@
 <html>
 
 <head>
-	<
-	<!--- NOT ALLOWED ... ONLY PHP AND VANILLA JS 
+	<!--- NOT ALLOWED ... ONLY PHP AND VANILLA JS .... ok plus a bit of CSS
 		<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script> 
 	-->
-	<!--- ok, can't do a prototype wihtout CSS so only PHP, JS and CSS .... -->
 	<link rel="stylesheet" href="css/main.css">
 </head>
 
 <body>
 	<center>
 	<code>
+	
 	<?php 
 	// get the file id for the json from the url
 	$file_id = htmlspecialchars($_GET["id"]);
@@ -27,8 +26,10 @@
 	$data = json_decode($content);
 	// build a main div to house the data
 	?><div class='main'><br><?php 
+	// get the parent data of the current element
 	// not sure if this needs a loop - could we have more than one decompose?
 	// if so would that change the structure of the array, so this wouldn't work anyway?
+	?><div style='float:none'><?php
 	foreach ($data->decomposes as $decomposes)
 	{
 		echo "<div class='decomposes'>";
@@ -39,22 +40,30 @@
 		echo "<a href='?id=".$id[4]."'>".$decomposes->type.'</a><br>';
 		echo '</div>';
 	}
+	echo '</div>';
+	// get the data for the current element
 	echo "<br><div style ='font-size:30px'>".$data->type.': '.$data->name.'</div>';
 	echo '<br>'.$data->globalId.'<br><br>';
 	
+	// get the child data of the current element
 	foreach ($data->isDecomposedBy[0] as $decomp)
 	{
 		echo "<div class='decombox'>";
 		$url = $decomp->ref;
+		// split the ref to get the filename
 		$id = preg_split("#/#", $url); 
 		echo '>> ';
-		// onclick send the fileid to the url...
+		// onclick send the filename to the url...
 		echo "<a href='?id=".$id[4]."'>".$decomp->type.'</a>';
 		echo '<br></div>';
 	}
 
-	?><br></div><?php
-	echo "<br><a href='?id=".$ifcProject."'>IfcProject</a>";
+	?><br></div>
+	<?php 
+	// add an escape route at the bottom in case it all goes crazy
+	echo "<br>Return to <a href='?id=".$ifcProject."'>IfcProject</a>";
+	// add credit
+	echo "<br><br>Based on a linked ifc JSON experiment by <a href='https://github.com/janbrouwer'>Jan Brouwer.</a>";
 	
 	?>
 	</code>
